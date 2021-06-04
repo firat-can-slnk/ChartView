8a9d70be72ed0c868b9337b05c325236cfb3c7b6
Firat <fsulunku@gmail.com>
2021-06-04 16:09:46 +0200

    changed IndicatorPoint

--- Sources/SwiftUICharts/LineChart/IndicatorPoint.swift
+++ Sources/SwiftUICharts/LineChart/IndicatorPoint.swift
@@ -9,15 +9,18 @@
 import SwiftUI
 
 struct IndicatorPoint: View {
+    let knobColor: Color = Colors.IndicatorKnob
+    let shadowColor: Color = Colors.LegendColor
+    
     var body: some View {
         ZStack{
             Circle()
-                .fill(Colors.IndicatorKnob)
+                .fill(knobColor)
             Circle()
                 .stroke(Color.white, style: StrokeStyle(lineWidth: 4))
         }
         .frame(width: 14, height: 14)
-        .shadow(color: Colors.LegendColor, radius: 6, x: 0, y: 6)
+        .shadow(color: shadowColor, radius: 6, x: 0, y: 3)
     }
 }
 
