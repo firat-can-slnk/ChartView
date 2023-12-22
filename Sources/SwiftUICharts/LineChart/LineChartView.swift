cd4ca905f129cc2ec7ed58a9f20e5417e5bd206a
Firat <fsulunku@gmail.com>
2023-12-23 00:51:46 +0100

    Revert "Corner Radius und Animation"
    This reverts commit ae14041c8b2a754c46becbc3bb3a7bbe2c920b98.

--- Sources/SwiftUICharts/LineChart/LineChartView.swift
+++ Sources/SwiftUICharts/LineChart/LineChartView.swift
@@ -77,10 +77,10 @@ public struct LineChartView: View {
                                 .offset(x: 0, y: 10)
                             Spacer()
                         }
-                        .transition(.opacity)
+                        .transition(.scale)
                         
                         selectionLabelView
-                            .padding(.top, formSize == ChartForm.medium.getSize() || formSize == ChartForm.extraLarge.getSize() ? 2 : 0)
+                            .padding(.top, 2)
                     }
                     Spacer()
                 }.frame(maxWidth: self.formSize.width, maxHeight: self.formSize.height)
@@ -102,7 +102,7 @@ public struct LineChartView: View {
                     }
                     .offset(y: showIndicatorDot ? -2 : 0)
                     .frame(minWidth: 0, idealWidth: frame.width, maxWidth: frame.width, minHeight: 0, idealHeight: frame.height, maxHeight: frame.height + (legend == nil && rateValue == nil ? 10 : 0) + additionalFrameHeight)
-                    .clipShape(RoundedRectangle(cornerRadius: 8))
+                    .clipShape(RoundedRectangle(cornerRadius: 20))
                 }
             }.frame(height: self.formSize.height)
         }
@@ -114,9 +114,7 @@ public struct LineChartView: View {
             self.getClosestDataPoint(toPoint: value.location, width:self.frame.width, height: self.frame.height)
         })
             .onEnded({ value in
-                withAnimation {
-                    self.showIndicatorDot = false
-                }
+                self.showIndicatorDot = false
             })
         )
     }
@@ -209,7 +207,7 @@ public struct LineChartView: View {
     }
     
     var rateValueView: some View {
-        Group
+        return Group
         {
             if let rateValue = rateValue
             {
